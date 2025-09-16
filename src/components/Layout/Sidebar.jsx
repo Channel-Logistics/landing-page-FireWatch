1a85c8296735f2574b1c8d77a8afe34395fb32a8
DavidSE03 <david@space-eyes.com>
2025-09-16 17:04:17 -0500

    fix: scroll sidebar

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -48,7 +48,7 @@ const Sidebar = ({
     );
   };
 
-  const navClasses = `flex-1 space-y-1.5 p-3 pb-3 ${mobile ? "pt-3" : "pt-4 mt-6"}`;
+  const navClasses = `flex-1 space-y-1.5 p-4 pb-4 ${mobile ? "pt-3" : "pt-4 mt-6"}`;
 
   const MenuContent = (
     <nav className={navClasses}>
@@ -146,7 +146,7 @@ const Sidebar = ({
 
 
   return (
-    <aside className="w-64 h-full bg-transparent border-r border-transparent flex flex-col">
+    <aside className="w-79 h-full bg-transparent border-r border-transparent flex flex-col">
       {MenuContent}
     </aside>
   );
